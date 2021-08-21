46be15704cb810258459ed5b1a62109c4ed1843e
brandon <brandondupitas@gmail.com>
2021-08-21 11:18:59 -0700

    uninstalled react-player

--- frontend/src/components/Body.js
+++ frontend/src/components/Body.js
@@ -1,10 +1,9 @@
 import React from "react";
-import ReactPlayer from "react-player/lazy";
 
 const Body = () => {
   return (
     <>
-      <ReactPlayer url="https://www.youtube.com/watch?v=GwaRztMaoY0" />
+     
     </>
   );
 };
