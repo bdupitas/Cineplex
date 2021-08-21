7d4363b1c4d20619a2615726a1290ffe0ace0237
brandon <brandondupitas@gmail.com>
2021-08-21 10:29:52 -0700

    abstracted components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import './App.css';
-import Body from './components/Body';
+import React from "react";
+import "./App.css";
+import Body from "./components/Body";
+import Navbar from "./components/NavBar";
 
-const App = () =>{
-  return <Body />
-}
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 
-
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Body />
+    </>
+  );
+};
 
 export default App;
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,16 +1,19 @@
 import React from "react";
+import { Menubar } from "primereact/menubar";
+import { PrimeIcons } from "primereact/api";
 
-
-
+const items = [
+  { Label: "Home", icon: PrimeIcons.HOME },
+  { Label: "Settings", icon: PrimeIcons.COG },
+  { Label: "Power-Off", icon: PrimeIcons.POWER_OFF }
+];
 
 const Navbar = () => {
-    return (
-        <>
-        </>
-    );
-}
+  return (
+    <>
+      <Menubar model={items} />
+    </>
+  );
+};
 
-
-
-
-export default Navbar
+export default Navbar;
