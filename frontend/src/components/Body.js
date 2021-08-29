796ec98dd462a82a256ff5fa6e42f967e513ee01
brandon <brandondupitas@gmail.com>
2021-08-29 12:52:04 -0700

    CDN allowed video to load rather than a remote service, will research more about CDNs

--- frontend/src/components/Body.js
+++ frontend/src/components/Body.js
@@ -1,26 +1,36 @@
 import React from "react";
 import VideoPlayer from "./VideoPlayer";
 
-
 const Body = () => {
+  const playerRef = React.useRef(null);
 
-
-  const videoJsOptions = { // lookup the options in the docs for more options
+  const videoJsOptions = {
+    // lookup the options in the docs for more options
     autoplay: true,
     controls: true,
     responsive: true,
     fluid: true,
-    sources: [{
-      src: 'testFiles/testMovie1.mp4',
-      type: 'video/mp4'
-    }]
-  }
+    sources: [
+      {
+        src: "//vjs.zencdn.net/v/oceans.mp4",
+        type: "video/mp4",
+      },
+    ],
+  };
 
-  return (
-    <>
-     <VideoPlayer options={videoJsOptions} />
-    </>
-  );
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // you can handle player events here
+    player.on("waiting", () => {
+      console.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      console.log("player will dispose");
+    });
+  };
+  return <VideoPlayer options={videoJsOptions} onReady={handlePlayerReady} />;
 };
 
 export default Body;
--- frontend/src/components/VideoPlayer.js
+++ frontend/src/components/VideoPlayer.js
@@ -1,35 +1,46 @@
 import React from "react";
 import videojs from "video.js";
 import "video.js/dist/video-js.css";
+import "../css/VideoPlayer.css";
 
-const VideoPlayer = (props) => {
+export const VideoPlayer = (props) => {
   const videoRef = React.useRef(null);
-  const { options } = props;
+  const playerRef = React.useRef(null);
+  const { options, onReady } = props;
 
-  // This seperate functional component fixes the removal of the videoelement
-  // from the DOM when calling the dispose() method on a player
-  const VideoHtml = (props) => (
+  React.useEffect(() => {
+    // make sure Video.js player is only initialized once
+    if (!playerRef.current) {
+      const videoElement = videoRef.current;
+      if (!videoElement) return;
+
+      const player = (playerRef.current = videojs(videoElement, options, () => {
+        console.log("player is ready");
+        onReady && onReady(player);
+      }));
+    } else {
+      // you can update player here [update player through props]
+      // const player = playerRef.current;
+      // player.autoplay(options.autoplay);
+      // player.src(options.sources);
+    }
+  }, [options]);
+
+  // Dispose the Video.js player when the functional component unmounts
+  React.useEffect(() => {
+    return () => {
+      if (playerRef.current) {
+        playerRef.current.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, []);
+
+  return (
     <div data-vjs-player>
       <video ref={videoRef} className="video-js vjs-big-play-centered" />
     </div>
   );
-
-  React.useEffect(() => {
-    const videoElement = videoRef.current;
-    let player;
-    if (videoElement) {
-      player = videojs(videoElement, options, () => {
-        console.log("player is ready");
-      });
-    }
-    return () => {
-      if (player) {
-        player.dispose();
-      }
-    };
-  }, [options]);
-
-  return <VideoHtml />;
 };
 
 export default VideoPlayer;
