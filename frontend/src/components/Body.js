e57e7398955d2f53553529aa845c2b4a93345b94
brandon <brandondupitas@gmail.com>
2021-08-29 12:12:43 -0700

    pushing local commits to branch

--- frontend/src/components/Body.js
+++ frontend/src/components/Body.js
@@ -1,9 +1,24 @@
 import React from "react";
+import VideoPlayer from "./VideoPlayer";
+
 
 const Body = () => {
+
+
+  const videoJsOptions = { // lookup the options in the docs for more options
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [{
+      src: 'testFiles/testMovie1.mp4',
+      type: 'video/mp4'
+    }]
+  }
+
   return (
     <>
-     
+     <VideoPlayer options={videoJsOptions} />
     </>
   );
 };
